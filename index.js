fdbcf65b7cf7a781dc70c2e516d22c8ebb9e6f79
Abdul Are <abdul0120@gmail.com>
2024-12-10 22:46:48 -0800

    Agregado script de inicio en package.json

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+
+// // Middleware para parsear el cuerpo de las peticiones como JSON
+// app.use(bodyParser.json());
+
+// // Ruta para probar que el servidor funciona
+// app.get('/', (req, res) => {
+//   res.send('Servidor funcionando correctamente');
+// });
+
+// // Ruta para recibir datos del ESP32
+// app.post('/sonido', (req, res) => {
+//   const soundLevel = req.body.soundLevel;
+//   console.log('Nivel de sonido recibido:', soundLevel);
+
+//   // Aquí puedes procesar o almacenar el dato recibido
+//   res.send({ status: 'OK', soundLevelReceived: soundLevel });
+// });
+
+// // Puerto dinámico proporcionado por Railway o fallback a 3000
+// const port = process.env.PORT || 3000;
+
+// // Inicia el servidor
+// app.listen(port, () => {
+//   console.log(`Servidor corriendo en el puerto ${port}`);
+// });
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const port = process.env.PORT || 3000;  // Usa el puerto asignado por Railway, o el puerto 3000 si no está disponible
 
 // Middleware para parsear el cuerpo de las peticiones como JSON
 app.use(bodyParser.json());
@@ -19,9 +50,6 @@ app.post('/sonido', (req, res) => {
   // Aquí puedes procesar o almacenar el dato recibido
   res.send({ status: 'OK', soundLevelReceived: soundLevel });
 });
-
-// Puerto dinámico proporcionado por Railway o fallback a 3000
-const port = process.env.PORT || 3000;
 
 // Inicia el servidor
 app.listen(port, () => {
